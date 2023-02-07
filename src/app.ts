666f616c2e237006517299c1323afb8d7ba10132
Adryan Freitas <adryansfreitas@gmail.com>
2023-02-07 11:42:57 -0300

    Add Logs

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import cors from 'cors';
-import express, { Express } from 'express';
+import express, { Express, Request } from 'express';
 import helmet from 'helmet';
+import morgan from 'morgan';
 
 import { connect as dbConnect } from './database';
 import routes from './routes';
@@ -19,6 +20,16 @@ class App {
     this.server.use(cors());
     this.server.use(helmet());
     this.server.use(express.json());
+
+    morgan.token('body', (req: Request) => {
+      return JSON.stringify(req.body);
+    });
+
+    this.server.use(
+      morgan(
+        `:remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent" \nBody: :body\n`,
+      ),
+    );
   }
 
   routes() {
